3c9f0859d7bc29dd4d5f6058d7aa837e7189ca0d
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-09-04 09:24:37 -0300

    stylos inline

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
+      <SafeAreaView style={{ backgroundcolor: "lightgreen" }}>
         <StatusBar style="auto"></StatusBar>
         <View>
           <Text>Dá hora filmes</Text>
