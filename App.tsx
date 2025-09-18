34cfff5c15729ef13694969c39e219fc96e7a1c4
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-09-18 09:21:27 -0300

    importa função layout

--- App.tsx
+++ App.tsx
@@ -12,21 +12,13 @@ import {
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 // Importando a biblioteca interna vector-icons
-
+// usamo o @tsignore para ignorar erros de tipagem(ou  ausencia de tipagem)
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
 import Loading from "./src/componentes/Loading";
 
 export default function App() {
   /* Usamos o useFonts para criar uma referência para a fonte que queremos uilizar no app. */
-  const [fonteCarregada] = useFonts({
-    Monoton: require("./assets/fonts/Monoton-Regular.ttf"),
-  });
-
-  // Se a fonte ainda não foi carregada
-  if (!fonteCarregada) {
-    return <Loading />;
-  }
 
   return (
     <SafeAreaProvider>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,16 @@
 import { View, Text } from "react-native";
-import React from "react";
+import { useFonts } from "expo-font";
+import Loading from "../src/componentes/Loading";
 
 export default function Layout() {
+  const [fonteCarregada] = useFonts({
+    Monoton: require("../assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  // Se a fonte ainda não foi carregada
+  if (!fonteCarregada) {
+    return <Loading />;
+  }
   return (
     <View>
       <Text>_layout</Text>
