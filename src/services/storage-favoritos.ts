6aa82c7f4797ab46c12d8a8b288dd46e69b06049
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-09-26 11:15:18 -0300

    cria funções de carregar , salvar e favoritar  com o async

---
+++ src/services/storage-favoritos.ts
@@ -0,0 +1,56 @@
+// src/services/storage-favoritos.ts
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Filme, FilmeDetalhes } from "../types";
+
+const STORAGE_FAVORITOS = "filmes_favoritos";
+
+/**
+ * le a lista e favoritos do async storage
+ * retona um array vazio em caso de erro ou chave inesistente
+ */
+async function carregar(): Promise<Filme[]> {
+  try {
+    //le a string salva na storage (pode vir null)
+    const favoritosArmazenamentos = await AsyncStorage.getItem(
+      STORAGE_FAVORITOS
+    );
+    // havendo favoritosArmazenamentos, converte a string em array e retorna convertido para objeto
+    // senao, retorna array vazio
+    return favoritosArmazenamentos ? JSON.parse(favoritosArmazenamentos) : [];
+  } catch (error) {
+    console.error("erro ao ler storage: " + error);
+
+    return [];
+  }
+}
+
+async function salvarLista(lista: Filme[]): Promise<void> {
+  try {
+    await AsyncStorage.setItem(STORAGE_FAVORITOS, JSON.stringify(lista));
+
+    //retorna true sinalizando no storage
+  } catch (error) {
+    console.error("Erro ao salvar filme");
+  }
+}
+
+/**
+ * Salvar um file na lista de favoritos (proprio storage)
+ * retorna true se salvou com sucesso, false se o filme ja estava nos favoritos
+ */
+
+async function slvarFilmeFavorito(filme: Filme): Promise<boolean> {
+  const favoritos = await carregar();
+  //verifica dse o filme ja existe na lista com o mesmo id na sta/storagede favoritos.a função some() retorna true se encontrar pelo menios 1item
+  if (favoritos.some((filmeExistente) => filmeExistente.id === filme.id)) {
+    return false;
+  }
+  // se chegou ate aqui é por que o filme nao havia sido salvo portanto adicionamos à lisa de favotos
+  favoritos.push(filme);
+
+  //salva a lista atualizada do storage
+  await salvarLista(favoritos);
+
+  return true;
+}
