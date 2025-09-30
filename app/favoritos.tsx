c3aa69bbba59f3bde5f21a99ccd923f3e4e34692
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-09-30 08:40:58 -0300

    configura parte do excluir todos

--- app/favoritos.tsx
+++ app/favoritos.tsx
@@ -78,6 +78,13 @@ export default function Favoritos() {
     }
   };
 
+  const apagarTudo = () => {
+    Alert.alert(
+      "Apagar todos os favoritos",
+      "Tem certeza que deseja apagar todos os favoritos"
+    );
+  };
+
   return (
     <>
       <Stack.Screen
