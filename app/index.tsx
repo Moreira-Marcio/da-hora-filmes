791269bf607cf87c817e70924ae27bf1b8905ed0
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-10-02 11:11:40 -0300

    muda frase e cor de botao

--- app/index.tsx
+++ app/index.tsx
@@ -45,7 +45,7 @@ export default function index() {
           <Link href="/sobre" asChild>
             <Pressable style={estilos.botaoRodape}>
               <Ionicons name="information-circle" size={18} color="white" />
-              <Text style={estilos.textoBotao}> Sobre</Text>
+              <Text style={estilos.textoBotao}> Sobreo app</Text>
             </Pressable>
           </Link>
         </View>
@@ -83,7 +83,7 @@ const estilos = StyleSheet.create({
     alignItems: "center",
   },
   botaoInicial: {
-    backgroundColor: "#5451a6",
+    backgroundColor: "#3c3c4b",
     padding: 16,
     borderRadius: 8,
     flexDirection: "row",
